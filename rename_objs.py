e5402b3931b95911038e5a72fc5538c0c931c1c4
Florian <florian.noichl@tum.de>
2020-10-26 14:09:57 +0100

    Revert "roundup w/ dummy blend files and extended main and appender scripts"
    This reverts commit 2136d92c

--- rename_objs.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-for collection in bpy.data.collections:
-    j = 1
-    for obj in collection.objects:
-        obj.name = collection.name + str(j)
-
-bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
