9683bb97e0673a3ec3ec7b3d6630f1a9f49437f0
Florian <florian.noichl@tum.de>
2020-10-26 12:04:28 +0100

    roundup w/ appender, main, renamer (new)

---
+++ rename_objs.py
@@ -0,0 +1,9 @@
+import bpy
+
+for collection in bpy.data.collections:
+    j = 1
+
+    for obj in collection.objects:
+        obj.name = collection.name + str(j)
+
+bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
