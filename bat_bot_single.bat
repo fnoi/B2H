4f5ef22ac4f810f5e142eadb3a3f3707057ce149
Florian <florian.noichl@tum.de>
2020-11-27 17:28:23 +0100

    !absolute path still

--- bat_bot_single.bat
+++ bat_bot_single.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo starting
-for %%f in (*.obj) do "C:\Program Files\VCG\MeshLab\meshlabserver.exe" -i "%%f" -o "%%~nf_simp.obj" -s "C:\Users\ga25mal\00_Work\06_meshlab\filter_simplify_1.mlx"
+for %%f in (*.obj) do "C:\Program Files\VCG\MeshLab\meshlabserver.exe" -i "%%f" -o "%%~nf_simp.obj" -s "C:\Users\ga25mal\PycharmProjects\blender_helpers\filter_simplify_1.mlx"
 
 echo done
 pause
